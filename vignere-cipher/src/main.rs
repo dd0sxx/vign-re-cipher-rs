cb055d6156936bca83f79543d8af9614ef0ec8f4
dd0sxx <contacttheotelonis@gmail.com>
2022-11-21 22:50:23 -0700

    pve with the compiler

--- vignere-cipher/src/main.rs
+++ vignere-cipher/src/main.rs
@@ -6,43 +6,49 @@ use dialoguer::{
 };
 use console::Term;
 
-
 fn main() {
 
     println!("Please choose from the following menu.");
     let selected = select_screen();
-    let mut message = String::new();
     let mut key = String::new();
-
+    
     match selected {
         Ok(val) => {
             if val == "Encode" {
+                let mut message = String::new();
                 println!("Please input your message.");
                 io::stdin()
-                    .read_line(&mut message)
-                    .expect("Failed to read line");
+                .read_line(&mut message)
+                .expect("Failed to read line");
                 
-    
+                
                 println!("Please input your secret encoding key.");
                 io::stdin()
-                    .read_line(&mut key)
-                    .expect("Failed to read line");
+                .read_line(&mut key)
+                .expect("Failed to read line");
+
+                let res = encode_vignere(message, key);
+                println!("your ciphertext ser: {}", res);
             } else {
+                let mut ciphertext = String::new();
                 println!("Please input your ciphertext.");
                 io::stdin()
-                    .read_line(&mut message)
-                    .expect("Failed to read line");
+                .read_line(&mut ciphertext)
+                .expect("Failed to read line");
                 
-    
+                
                 println!("Please input the secret decoding key.");
                 io::stdin()
-                    .read_line(&mut key)
-                    .expect("Failed to read line");
+                .read_line(&mut key)
+                .expect("Failed to read line");
+                
+                let res = decode_vignere(ciphertext, key);
+                println!("your message ser: {}", res);
             }
         },
         Err(err) => {
             // Do something with the error if you want
-            println!("Invalid selection...");
+            println!("Error: {}", err);
         }
     }
 
@@ -51,16 +57,33 @@ fn main() {
     
 }
 
-fn encode_vignere(m: String, k: String) {
+const ALPHABET: String = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".to_string();
 
+fn char_to_pos(input: char) -> usize {
+    ALPHABET.chars().position(|c| input == c).unwrap()
 }
 
-fn decode_vignere(c: String, k: String) {
 
+fn encode_vignere(m: String, k: String) -> String {
+    let mut result: String = "".to_string();
+    for message_char in m.chars() {
+        for key_char in k.chars() {
+            let new_char_index = char_to_pos(message_char) + char_to_pos(key_char);
+            let new_char = ALPHABET.chars().nth(new_char_index).unwrap();
+            result.push(new_char);
+        }
+    }
+    result
+}
+
+fn decode_vignere(c: String, k: String) -> String {
+    let mut result: String = "".to_string();
+
+    result
 }
 
 fn select_screen() -> std::io::Result<(String)> {
-        let items = vec!["Encode", "Decode"];
+    let items = vec!["Encode", "Decode"];
     let selection = Select::with_theme(&ColorfulTheme::default())
         .items(&items)
         .default(0)
